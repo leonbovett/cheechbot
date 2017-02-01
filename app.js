6b12f5a73c837acdcc00958462556e578582f69a
Leon Bovett <leon.bovett@accenture.com>
2017-02-01 17:49:48 +0000

    try using qna instead of luis

--- app.js
+++ app.js
@@ -23,14 +23,32 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/0bd654cf-6de3-4e9c-a611-096414a65eb2?subscription-key=67630055817641179c82295f202cbe19&q=design%20strategies';
-var recognizer = new builder.LuisRecognizer(model);
-var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
-bot.dialog('/', dialog);
 
-// Add intent handlers
-dialog.matches('responsive design', builder.DialogAction.send("Have you tried creating a responsive design?"));
-dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand."));
+
+var recognizer = new cognitiveservices.QnAMakerRecognizer({
+	knowledgeBaseId: process.env.QNA_KNOWLEDGEBASE_ID,
+	subscriptionKey: process.env.QNA_SUBSCRIPTION_KEY
+});
+
+var BasicQnAMakerDialog = new cognitiveservices.QnAMakerDialog({
+	recognizers: [recognizer],
+	defaultMessage: 'No good match in FAQ.',
+	qnaThreshold: 0.5});
+
+bot.dialog('/', BasicQnAMakerDialog);
+
+
+// var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/0bd654cf-6de3-4e9c-a611-096414a65eb2?subscription-key=67630055817641179c82295f202cbe19&q=design%20strategies';
+// var recognizer = new builder.LuisRecognizer(model);
+// var dialogDesignStrategies = new builder.IntentDialog({ recognizers: [recognizer] });
+// bot.dialog('/', dialogDesignStrategies);
+//
+// // Add intent handlers
+// dialog.matches('responsive design', builder.DialogAction.send("Have you tried creating a responsive design?"));
+// dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand."));
+
+
+
 
 
 // bot.dialog('/', function (session) {
